fbc9a1e57b2e40ec0763c693d40eed6e489803e2
Craig Lu <Craig.Z.Lu@gmail.com>
2016-04-18 02:49:56 -0400

    Fixed textbox and tested item disappear

--- Assets/Scripts/Dialog/NPCController_Dave.cs
+++ Assets/Scripts/Dialog/NPCController_Dave.cs
@@ -19,6 +19,7 @@ public class NPCController_Dave : MonoBehaviour {
     private string item;
     public ItemType questItem;
     public int questID;
+    private bool QuestDone;
 
 	// Use this for initialization
 	void Start() {
@@ -46,6 +47,7 @@ public class NPCController_Dave : MonoBehaviour {
             case ItemType.BOTTLE:
                 item = "Bottle";
                 break;
+
         }
 	}
 
@@ -80,23 +82,35 @@ public class NPCController_Dave : MonoBehaviour {
 
 	void Talk() {
 		movementScript.SetTarget(player.transform.position);
-        if (quest.questID == 0 || quest.questID == questID) 
+        if (QuestDone)
+        {
+            modalPanel.Choice("Thanks for helping me out!");
+        }
+        else if (quest.questID == 0 || quest.questID == questID) 
         {
             if (!questComplete && !questInProgress)
             {
                 if (questType == "Kill Quest")
                 {
-                    modalPanel.Choice("These monsters have been causing so much trouble! Can you get rid of " + questGoal + " monster?", AcceptQuest, DeclineQuest);
+                    modalPanel.Choice("These monsters have been causing so much trouble! Can you get rid of " + questGoal + " monsters?", AcceptQuest, DeclineQuest);
                 }
                 else
                 {
-                    modalPanel.Choice("There has been a lot of danger around here lately I haven't been able to collect any " + item + " s. Could you help me collect " + questGoal + " " + item + "s?", AcceptQuest, DeclineQuest);
+                    modalPanel.Choice("There has been a lot of danger around here lately I haven't been able to collect any " + item + "s. Could you help me collect " + questGoal + " " + item + "s?", AcceptQuest, DeclineQuest);
                 }
                 //modalPanel.Choice("Would you like to start this quest?", AcceptQuest, DeclineQuest);
             }
             else if (!questComplete && questInProgress)
             {
-                modalPanel.Choice("Have you finished my task? Oh... okay come back when its finished.");
+                if (questType == "Kill Quest")
+                {
+                    modalPanel.Choice("Have you killed " + questGoal + " monsters yet? Oh... well come back when you finish.");
+                }
+                else
+                {
+                    modalPanel.Choice("Have you collected " + questGoal + " " + item + "s yet? Oh... well come back when you finish.");
+                }
+                    
             }
             else if (questComplete)
             {
@@ -106,6 +120,7 @@ public class NPCController_Dave : MonoBehaviour {
 
                     questInProgress = false;
                     questComplete = false;
+                    QuestDone = true;
                     quest.QuestComplete = questComplete;
                     quest.questID = 0;
                     quest.count = 0;
@@ -115,6 +130,7 @@ public class NPCController_Dave : MonoBehaviour {
                     modalPanel.Choice("Thanks for all the " + item + "s! I can finally get back to work!\nAllow me to show my appreciation. Take this.");
                     questInProgress = false;
                     questComplete = false;
+                    QuestDone = true;
                     quest.QuestComplete = questComplete;
                     quest.questID = 0;
                     quest.count = 0;
