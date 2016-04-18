fadf95348c960244e2007029cf1f1631eb331249
Ritwik Shokeen <inritwik12085@iiitd.ac.in>
2016-04-18 04:20:44 -0400

    added gameover scene and enemy2

---
+++ Assets/Scripts/enemy2visioncone.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+
+public class enemy2visioncone : MonoBehaviour {
+
+	public GameObject player;
+	public float farVisionCone;
+	public float farDistance;
+	public float closeVisionCone;
+	public float closeDistance;
+	private Boolean canSee;
+	private Boolean canAttack;
+	private Vector3 direction;
+	void Update() {
+		Vector3 targetDir = player.transform.position - transform.position;
+		Vector3 direction = this.gameObject.GetComponent<enemy2AIMovement> ().getDirection ();
+		float distance = Vector3.Distance(player.transform.position, transform.position);
+		float angle = Vector3.Angle(targetDir, direction);
+		if (angle <= closeVisionCone && distance <= closeDistance) {
+			canAttack = true;
+			canSee = true;
+		} else if (angle <= farVisionCone && distance <= farDistance) {
+			canSee = true;
+			canAttack = false;
+		} 
+		else {
+			canSee = false;
+			canAttack = false;
+		}
+	}
+	public Boolean getCanAttack(){
+		return canAttack;
+	}
+	public Boolean getCanSee(){
+		return canSee;
+	}
+}
---
+++ Assets/Scripts/gameoverscript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameoverscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void mainmenu() {
+		Application.LoadLevel ("Main Menu");
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+
+}
