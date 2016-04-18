b28aebfb90cefd86a2e4c64509b04f74dc9f21b8
MeganMackay <meg.souggie@ufl.edu>
2016-04-18 04:23:58 -0400

    ai movement script for type 1 and 3 enemies

---
+++ Assets/Scripts/AIMovementType1.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+using System.Xml.Linq;
+using UnityEditor;
+using System.Security.Cryptography;
+
+public class AIMovementType1 : MonoBehaviour {
+
+	//private Animator animator;
+	public GameObject player;
+	public float speed;
+	private VisionCone vision;
+	private Vector3 pos;
+	private Vector3 target;
+	private float nextPath;
+	private Vector3 direction;
+	void Start () {
+		//animator = this.GetComponent<Animator> ();
+		vision = this.GetComponent<VisionCone> ();
+		target = new Vector3 (0, 0, 0);
+		nextPath = Time.time;
+	}
+
+	void Update () {
+		float rand = Random.value;
+		if (!this.gameObject.GetComponent<Rigidbody2D> ().IsAwake ()) {
+			pos = this.transform.position;
+		}
+		if(vision.getCanSee ()){
+			target = player.transform.position;
+			nextPath = Time.time + 2;
+		}
+		else if(rand <= .25 && !this.gameObject.GetComponent<Rigidbody2D> ().IsAwake () && Time.time > nextPath) {
+			target = pos + new Vector3 (0, 5, 0);
+			this.GetComponent<Animator> ().SetInteger ("E1Direction", 3);
+			nextPath = Time.time + 2;
+			direction = transform.up;
+		}
+		else if(rand <= .50 && !this.gameObject.GetComponent<Rigidbody2D> ().IsAwake () && Time.time > nextPath) {
+			target = pos + new Vector3 (5, 0, 0);
+			this.GetComponent<Animator> ().SetInteger ("E1Direction", 2);
+			nextPath = Time.time + 2;
+			direction = transform.right;
+		}
+		else if(rand <= .75 && !this.gameObject.GetComponent<Rigidbody2D> ().IsAwake () && Time.time > nextPath) {
+			target = pos + new Vector3 (0, -5, 0);
+			this.GetComponent<Animator> ().SetInteger ("E1Direction", 2);
+			nextPath = Time.time + 2;
+			direction = -transform.up;
+		}
+		else if(rand <= 1.0 && !this.gameObject.GetComponent<Rigidbody2D> ().IsAwake () && Time.time > nextPath) {
+			target = pos + new Vector3 (-5, 0, 0);
+			this.GetComponent<Animator> ().SetInteger ("E1Direction", 2);
+			nextPath = Time.time + 2;
+			direction = -transform.right;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+	}
+	public Vector3 getDirection(){
+		return direction;
+	}
+}
